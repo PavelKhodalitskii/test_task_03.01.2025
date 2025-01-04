6526cdc533058f2e0ad6aad452bc77c18bf236e9
PavelKhodalitskii <dreamsobenatic00@mail.ru>
2025-01-04 12:11:13 +0800

    Registration view created

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import LoginUser, logout_user
+from .views import LoginUser, logout_user, RegisterUser
 
 urlpatterns = [
     path("login/", LoginUser.as_view(), name="login"),
-    path("logout/", logout_user, name="logout")
+    path("logout/", logout_user, name="logout"),
+    path("register/", RegisterUser.as_view(), name="registration")
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,28 @@
 from django.shortcuts import redirect
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import FormView
 from django.urls import reverse_lazy
 
+from .forms import UserRegistrationForm
 
 class LoginUser(auth_views.LoginView):
     form_class = AuthenticationForm
     template_name = 'accounts/login.html'
 
+    def get_success_url(self):
+        return reverse_lazy('root')
+
+class RegisterUser(FormView):
+    form_class = UserRegistrationForm
+    template_name = 'accounts/register.html'
+
+    def form_valid(self, form):
+        new_user = form.save()
+        login(request=self.request, user=new_user)
+        return super().form_valid(form)
+    
     def get_success_url(self):
         return reverse_lazy('root')
 
